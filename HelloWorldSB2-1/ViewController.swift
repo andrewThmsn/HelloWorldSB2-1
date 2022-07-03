218b5ef20b223165c003be6a408077fe66f54b8b
andrewThmsn <andtaravatar10@gmail.com>
2022-07-03 23:00:44 +0300

    Add action button

--- HelloWorldSB2-1/ViewController.swift
+++ HelloWorldSB2-1/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var helloWorldLabel: UILabel!
+    
+    @IBOutlet var toggleButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        helloWorldLabel.isHidden = true
+        toggleButton.layer.cornerRadius = 10
+        
     }
 
+    @IBAction func makeButtonLabel(_ sender: Any) {
+        if helloWorldLabel.isHidden {
+            helloWorldLabel.isHidden = false
+            toggleButton.setTitle("Hide Text", for: .normal)
+        } else {
+            helloWorldLabel.isHidden = true
+            toggleButton.setTitle("Show Text", for: .normal)
+            }
+        }
+    }
+    
 
-}
 
